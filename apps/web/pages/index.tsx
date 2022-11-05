074b8b0ccfc80597062feb405c9186fb8fc45e73
Leonardo Almeida Marques <leo@sydl1500661.oc.rabonet.com>
2022-11-05 21:01:14 +0100

    feat: :white_check_mark: added jest config and mocking tests

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -3,8 +3,8 @@ import { Button } from "ui";
 export default function Web() {
   return (
     <div>
-      <h1>Web</h1>
-      <Button />
+      <h1>WEB</h1>
+      <Button onClick={() => console.log("web button clicked")}>Hello</Button>
     </div>
   );
 }
--- packages/ui/components/button/Button.stories.tsx
+++ packages/ui/components/button/Button.stories.tsx
@@ -1,7 +1,6 @@
-import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
-import { Button } from "./Button";
+import { Button } from ".";
 
 export default {
   title: "Example/Button",
